8f54a5256df3f8f349351a9af3ee135d8921bf81
Klix <33373965+Klixxx@users.noreply.github.com>
2019-09-02 21:33:06 +0200

    Updated armory mode

---
+++ ElvUI_KlixUI/modules/armory/armory.lua
@@ -0,0 +1,431 @@
+local KUI, T, E, L, V, P, G = unpack(select(2, ...))
+local KA = KUI:NewModule('KuiArmory', 'AceEvent-3.0', 'AceTimer-3.0', 'AceHook-3.0')
+local LCG = LibStub('LibCustomGlow-1.0')
+local LSM = E.LSM or E.Libs.LSM
+
+local initialized = false
+local updateTimer
+
+local slots = {
+	["HeadSlot"] = { true, true },
+	["NeckSlot"] = { true, false },
+	["ShoulderSlot"] = { true, true },
+	["BackSlot"] = { true, false },
+	["ChestSlot"] = { true, true },
+	["WristSlot"] = { true, true },
+	["MainHandSlot"] = { true, true },
+	["SecondaryHandSlot"] = { true, true },
+	["RangedSlot"] = { true, true },
+	["AmmoSlot"] = { true, true },
+	["HandsSlot"] = { true, true },
+	["WaistSlot"] = { true, true },
+	["LegsSlot"] = { true, true },
+	["FeetSlot"] = { true, true },
+	["Finger0Slot"] = { true, false },
+	["ShirtSlot"] = { false, false },
+	["Finger1Slot"] = { true, false },
+	["TabardSlot"] = { false, false },
+	["Trinket0Slot"] = { true, false },
+	["Trinket1Slot"] = { true, false },
+	
+}
+
+local slotIDs = {
+	[1] = "HeadSlot",
+	[2] = "NeckSlot",
+	[3] = "ShoulderSlot",
+	[5] = "ChestSlot",
+	[6] = "ShirtSlot",
+	[7] = "TabardSlot",
+	[8] = "WaistSlot",
+	[9] = "LegsSlot",
+	[10] = "FeetSlot",
+	[11] = "WristSlot",
+	[12] = "HandsSlot",
+	[13] = "Finger0Slot",
+	[14] = "Finger1Slot",
+	[15] = "Trinket0Slot",
+	[16] = "Trinket1Slot",
+	[17] = "BackSlot",
+	[18] = "MainHandSlot",
+	[19] = "SecondaryHandSlot",
+	[20] = "RangedSlot",
+	[21] = "AmmoSlot",
+}
+
+local levelColors = {
+	[0] = "|cffff0000",
+	[1] = "|cff00ff00",
+	[2] = "|cffffff88",
+}
+
+function KA:UpdatePaperDoll()
+	if not E.db.KlixUI.armory.enable then return end
+
+	local unit = "player"
+	if not unit then return end
+
+	local frame, slot, current, maximum, r, g, b
+	local itemLink, itemLevel, itemLevelMax, enchantInfo
+	local _, numBonuses, affixes
+	local EnchantGlowColor = {KA.db.indicators.enchant.glow.color.r, KA.db.indicators.enchant.glow.color.g, KA.db.indicators.enchant.glow.color.b, KA.db.indicators.enchant.glow.color.a or 1}
+	local SocketGlowColor = {KA.db.indicators.socket.glow.color.r, KA.db.indicators.socket.glow.color.g, KA.db.indicators.socket.glow.color.b, KA.db.indicators.socket.glow.color.a or 1}
+
+	for k, _ in T.pairs(slots) do
+		frame = _G[("Character")..k]
+		slot = T.GetInventorySlotInfo(k)
+		if slot and slot ~= '' then
+			-- Reset Data first
+			--[[if not E.db.general.itemLevel.displayCharacterInfo then
+				frame.ItemLevel:SetText("")
+			end]]
+			frame.DurabilityInfo:SetText("")
+			--[[if not E.db.general.itemLevel.displayCharacterInfo then
+				if KA.db.indicators.enchant.glow.enable then
+					frame.EnchantInfo:Hide()
+				else
+					frame.EnchantInfo:SetText("")
+				end
+			end]]
+			frame.Gradiation:Hide()
+
+			itemLink = T.GetInventoryItemLink(unit, slot)
+			if (itemLink and itemLink ~= nil) then
+				if T.type(itemLink) ~= "string" then return end
+				_, _, _, _, _, _, _, _, _, _, _, _, _, numBonuses, affixes = T.string_split(":", itemLink, 15)
+				numBonuses = T.tonumber(numBonuses) or 0
+
+				local _, _, itemRarity, _, _, _, _, _, itemEquipLoc = T.GetItemInfo(itemLink)
+				-- Item Level
+				--[[if not E.db.general.itemLevel.displayCharacterInfo then
+					if KA.db.ilvl.enable then
+						if ((slot == 16 or slot == 17) and T.GetInventoryItemQuality(unit, slot) == LE_ITEM_QUALITY_ARTIFACT) then
+							local itemLevelMainHand = 0
+							local itemLevelOffHand = 0
+							local itemLinkMainHand = T.GetInventoryItemLink(unit, 16)
+							local itemLinkOffhand = T.GetInventoryItemLink(unit, 17)
+							if itemLinkMainHand then itemLevelMainHand = self:GetItemLevel(unit, itemLinkMainHand or 0) end
+							if itemLinkOffhand then itemLevelOffHand = self:GetItemLevel(unit, itemLinkOffhand or 0) end
+							itemLevel = T.math_max(itemLevelMainHand or 0, itemLevelOffHand or 0)
+						else
+							itemLevel = self:GetItemLevel(unit, itemLink)
+						end
+						if itemLevel and avgEquipItemLevel then
+							frame.ItemLevel:SetText(itemLevel)
+						end
+						if (slot == 4 or slot == 19) then
+							frame.ItemLevel:SetText("")
+						end
+						if itemRarity and KA.db.ilvl.colorStyle == "RARITY" then
+							local r, g, b = T.GetItemQualityColor(itemRarity)
+							frame.ItemLevel:SetTextColor(r, g, b)
+						elseif KA.db.ilvl.colorStyle == "LEVEL" then
+							frame.ItemLevel:SetFormattedText("%s%d|r", levelColors[(itemLevel < avgEquipItemLevel-10 and 0 or (itemLevel > avgEquipItemLevel + 10 and 1 or (2)))], itemLevel)
+						else
+							frame.ItemLevel:SetTextColor(KUI:unpackColor(KA.db.ilvl.color))
+						end
+					end
+
+					-- Enchants
+					if KA.db.indicators.enchant.enable then
+						if KA.db.indicators.enchant.glow.enable then
+							local enchantInfo = self:CheckEnchants(itemLink)
+							if enchantInfo then
+								if (slot == 11 or slot == 12 or slot == 16 or (slot == 17 and itemEquipLoc ~= 'INVTYPE_SHIELD' and itemEquipLoc ~= 'INVTYPE_HOLDABLE')) then
+									frame.EnchantInfo:Show()
+									frame.EnchantInfo:SetBackdropColor(0, 0, 0, 0)
+									LCG.AutoCastGlow_Stop(frame.EnchantInfo)
+									LCG.ButtonGlow_Stop(frame.EnchantInfo)
+									LCG.PixelGlow_Stop(frame.EnchantInfo)
+								end	
+							else
+								if (slot == 11 or slot == 12 or slot == 16 or (slot == 17 and itemEquipLoc ~= 'INVTYPE_SHIELD' and itemEquipLoc ~= 'INVTYPE_HOLDABLE')) then
+									frame.EnchantInfo:Show()
+									frame.EnchantInfo:SetBackdropColor(0, 0, 0, 0)
+									if KA.db.indicators.enchant.glow.style == "Button" then
+										LCG.PixelGlow_Stop(frame.EnchantInfo)
+										LCG.AutoCastGlow_Stop(frame.EnchantInfo)
+										LCG.ButtonGlow_Start(frame.EnchantInfo, EnchantGlowColor, 0.25)
+									elseif KA.db.indicators.enchant.glow.style == "Pixel" then
+										LCG.ButtonGlow_Stop(frame.EnchantInfo)
+										LCG.AutoCastGlow_Stop(frame.EnchantInfo)
+										LCG.PixelGlow_Start(frame.EnchantInfo, EnchantGlowColor, nil, 0.25, nil, 2)
+									elseif KA.db.indicators.enchant.glow.style == "AutoCast" then
+										LCG.ButtonGlow_Stop(frame.EnchantInfo)
+										LCG.PixelGlow_Stop(frame.EnchantInfo)
+										LCG.AutoCastGlow_Start(frame.EnchantInfo, EnchantGlowColor, 6, 0.25, 1.5)
+									end
+								end
+							end
+						else
+							local enchantInfo = self:GetEnchants(itemLink)
+							if enchantInfo then
+								if (slot == 11 or slot == 12 or slot == 16 or (slot == 17 and itemEquipLoc ~= 'INVTYPE_SHIELD' and itemEquipLoc ~= 'INVTYPE_HOLDABLE')) then
+									frame.EnchantInfo:SetText(enchantInfo)
+								end
+							end
+						end
+					end]]
+
+				-- Durability
+				if KA.db.durability.enable then
+					frame.DurabilityInfo:SetText()
+					current, maximum = T.GetInventoryItemDurability(slot)
+					if current and maximum and (not KA.db.durability.onlydamaged or current < maximum) then
+						r, g, b = E:ColorGradient((current / maximum), 1, 0, 0, 1, 1, 0, 0, 1, 0)
+						frame.DurabilityInfo:SetFormattedText("%s%.0f%%|r", E:RGBToHex(r, g, b), (current / maximum) * 100)
+					end
+				end
+				
+				-- Gradiation
+				if KA.db.gradient.enable then
+					frame.Gradiation:Show()
+					frame.Gradiation:SetAlpha(KA.db.gradient.alpha)
+					if itemRarity and KA.db.gradient.colorStyle == "RARITY" then
+						local r, g, b = T.GetItemQualityColor(itemRarity)
+						frame.Gradiation.Texture:SetVertexColor(r, g, b)
+					elseif KA.db.gradient.colorStyle == "VALUE" then
+						frame.Gradiation.Texture:SetVertexColor(T.unpack(E.media.rgbvaluecolor))
+					else
+						frame.Gradiation.Texture:SetVertexColor(KUI:unpackColor(KA.db.gradient.color))
+					end
+				end
+			end
+		end
+	end
+end
+
+-- from http://www.wowinterface.com/forums/showthread.php?p=284771 by PhanX
+-- Construct your search pattern based on the existing global string:
+-- local S_UPGRADE_LEVEL   = "^" .. T.string_gsub(ITEM_UPGRADE_TOOLTIP_FORMAT, "%%d", "(%%d+)")
+local S_ITEM_LEVEL      = "^" .. T.string_gsub(ITEM_LEVEL, "%%d", "(%%d+)")
+
+-- Create the tooltip:
+local scantip = T.CreateFrame("GameTooltip", "MyScanningTooltip", nil, "GameTooltipTemplate")
+scantip:SetOwner(E.UIParent, "ANCHOR_NONE")
+
+local function GetRealItemLevel(itemLink)
+	if not itemLink then return end
+	
+	-- Pass the item link to the tooltip
+	scantip:ClearLines()
+	scantip:SetHyperlink(itemLink)
+
+	-- Scan the tooltip:
+	for i = 2, scantip:NumLines() do -- Line 1 is always the name so you can skip it.
+		local text = _G["MyScanningTooltipTextLeft"..i]:GetText()
+		if text and text ~= "" then
+			local currentLevel = T.string_match(text, S_ITEM_LEVEL)
+			if currentLevel then
+				return currentLevel
+			end
+		end
+	end
+end
+
+function KA:GetItemLevel(unit, itemLink)
+	if not itemLink then return end
+
+	local rarity, itemLevel = T.select(3, T.GetItemInfo(itemLink))
+	if rarity == 7 then -- heirloom adjust
+		itemLevel = self:HeirLoomLevel(unit, itemLink)
+	end
+
+	itemLevel = GetRealItemLevel(itemLink)
+	itemLevel = T.tonumber(itemLevel)
+	return itemLevel
+end
+
+-- Check missing enchants
+--[[function KA:CheckEnchants(itemLink)
+	if not itemLink then return end
+	
+	-- Pass the item link to the tooltip
+	scantip:ClearLines()
+	scantip:SetHyperlink(itemLink)
+
+	for i = 1, scantip:NumLines() do
+		local enchant = _G["MyScanningTooltipTextLeft"..i]:GetText():match(ENCHANTED_TOOLTIP_LINE:gsub("%%s", "(.+)"))
+		if enchant then
+			return enchant
+		end
+	end
+end
+
+function KA:GetEnchants(itemLink)
+	if not itemLink then return end
+
+	local enchantInfo = self:CheckEnchants(itemLink)
+
+	if enchantInfo then
+		enchantInfo = "|cff00ff00E|r"
+	else
+		enchantInfo = "|cffff0000E|r"
+	end
+
+	return enchantInfo
+end]]
+
+function KA:InitialUpdatePaperDoll()
+	KA:UnregisterEvent("PLAYER_ENTERING_WORLD")
+
+	self:BuildInformation()
+
+	-- update player info
+	self:ScheduleTimer("UpdatePaperDoll", 2)
+
+	initialized = true
+end
+
+local function UpdateiLvLPoints(id)
+	if not id then return end
+	
+	if id <= 7 or id == 17 or id == 11 then 		-- Left side
+		return "BOTTOMLEFT", "BOTTOMLEFT", 1, 2
+	elseif id <= 16 then 							-- Right side
+		return "BOTTOMRIGHT", "BOTTOMRIGHT", 2, 2
+	else											-- Weapon slots
+		if KA.db.indicators.illusion.enable then
+			return "BOTTOM", "BOTTOM", 2, 6
+		else
+			return "BOTTOM", "BOTTOM", 2, 1
+		end
+	end
+end
+
+local function UpdateDurabilityPoints(id)
+	if not id then return end
+	
+	if id <= 7 or id == 17 or id == 11 then 		-- Left side
+		return "TOPRIGHT", "TOPRIGHT", 0, -3
+	elseif id <= 16 then 							-- Right side
+		return "TOPLEFT", "TOPLEFT", 2, -3
+	else											-- Weapon slots
+		return "TOPLEFT", "TOPLEFT", 2, -3
+	end
+end
+
+function KA:BuildInformation()
+	for id, slotName in T.pairs(slotIDs) do
+		if not id then return end
+		
+		local frame = _G["Character"..slotIDs[id]]
+		local iLvLPoint, iLvLParentPoint, x1, y1 = UpdateiLvLPoints(id)
+		local DuraPoint, DuraParentPoint, x2, y2 = UpdateDurabilityPoints(id)
+		
+		-- Item Level
+		--[[if not E.db.general.itemLevel.displayCharacterInfo then
+			frame.ItemLevel = frame:CreateFontString(nil, "OVERLAY")
+			frame.ItemLevel:SetPoint(iLvLPoint, frame, iLvLParentPoint, x1 or 0, y1 or 0)
+			frame.ItemLevel:FontTemplate(LSM:Fetch("font", KA.db.ilvl.font), KA.db.ilvl.textSize, KA.db.ilvl.fontOutline)
+		end]]
+
+		-- Durability
+		frame.DurabilityInfo = frame:CreateFontString(nil, "OVERLAY")
+		frame.DurabilityInfo:SetPoint(DuraPoint, frame, DuraParentPoint, x2 or 0, y2 or 0)
+		frame.DurabilityInfo:FontTemplate(LSM:Fetch("font", KA.db.durability.font), KA.db.durability.textSize, KA.db.durability.fontOutline)
+		
+		-- Enchant Info
+		--[[if not E.db.general.itemLevel.displayCharacterInfo then
+			if KA.db.indicators.enchant.glow.enable then
+				frame.EnchantInfo = T.CreateFrame('Frame', nil, frame)
+				frame.EnchantInfo:Size(37)
+				frame.EnchantInfo:SetBackdrop({
+					bgFile = E.media.blankTex,
+					edgeFile = E.media.blankTex,
+					tile = false, tileSize = 0, edgeSize = E.mult,
+					insets = { left = 0, right = 0, top = 0, bottom = 0}
+				})
+				frame.EnchantInfo:SetBackdropBorderColor(0, 0, 0, 0)
+				frame.EnchantInfo:SetPoint("CENTER", frame, "CENTER", 0, 0)
+			else
+				frame.EnchantInfo = frame:CreateFontString(nil, "OVERLAY")
+				frame.EnchantInfo:SetPoint("TOPRIGHT", frame, "TOPRIGHT", 1, -1)
+				frame.EnchantInfo:FontTemplate(LSM:Fetch("font", KA.db.durability.font), (KA.db.durability.textSize + 1), KA.db.durability.fontOutline)
+			end
+		end]]
+		
+		-- Gradiation
+		frame.Gradiation = T.CreateFrame('Frame', nil, frame)
+		frame.Gradiation:Size(120, 41)
+		frame.Gradiation:SetFrameLevel(_G["CharacterModelFrame"]:GetFrameLevel())
+
+		frame.Gradiation.Texture = frame.Gradiation:CreateTexture(nil, "OVERLAY")
+		frame.Gradiation.Texture:SetInside()
+		frame.Gradiation.Texture:SetTexture('Interface\\AddOns\\ElvUI_KlixUI\\media\\textures\\Gradation')
+
+		if id <= 7 or id == 17 or id == 11 then -- Left Side
+			frame.Gradiation:SetPoint("LEFT", _G["Character"..slotName], "RIGHT", -20, 0)
+			frame.Gradiation.Texture:SetTexCoord(0, 1, 0, 1)
+		elseif id <= 16 then -- Right Side
+			frame.Gradiation:SetPoint("RIGHT", _G["Character"..slotName], "LEFT", 20, 0)
+			frame.Gradiation.Texture:SetTexCoord(1, 0, 0, 1)
+		end
+	end
+end
+
+--[[function KA:UpdateIlvlFont()
+	_G["CharacterStatsPane"].ItemLevelFrame.Value:FontTemplate(LSM:Fetch('font', KA.db.stats.ItemLevel.font), KA.db.stats.ItemLevel.size or 12, KA.db.stats.ItemLevel.outline)
+	_G["CharacterStatsPane"].ItemLevelFrame:SetHeight((KA.db.stats.ItemLevel.size or 12) + 4)
+	_G["CharacterStatsPane"].ItemLevelFrame.Background:SetHeight((KA.db.stats.ItemLevel.size or 12) + 4)
+	if _G["CharacterStatsPane"].ItemLevelFrame.leftGrad then
+		_G["CharacterStatsPane"].ItemLevelFrame.leftGrad:SetHeight((KA.db.stats.ItemLevel.size or 12) + 4)
+		_G["CharacterStatsPane"].ItemLevelFrame.rightGrad:SetHeight((KA.db.stats.ItemLevel.size or 12) + 4)
+	end
+end]]
+
+function KA:ResizeCharacterFrame()
+	if _G["PaperDollFrame"]:IsShown() then
+		_G["CharacterFrame"]:SetWidth(415)
+		_G["CharacterFrame"]:SetHeight(530)
+
+		_G.CharacterHandsSlot:ClearAllPoints()
+		_G.CharacterHandsSlot:SetPoint("TOPRIGHT", _G.CharacterFrame.backdrop, "TOPRIGHT", -10, -62)
+		
+		_G.CharacterMainHandSlot:ClearAllPoints()
+		_G.CharacterMainHandSlot:SetPoint("BOTTOMLEFT", _G.CharacterFrame.backdrop, "BOTTOMLEFT", 125, 10)
+		
+		_G.CharacterModelFrame:SetSize(250, 250)
+		_G.CharacterModelFrame:ClearAllPoints()
+		_G.CharacterModelFrame:SetPoint("CENTER", _G.CharacterFrame.backdrop, "CENTER", 0, 30)
+		
+		_G.CharacterModelFrameRotateLeftButton:ClearAllPoints()
+		_G.CharacterModelFrameRotateLeftButton:SetPoint("TOPLEFT", _G.CharacterHeadSlot, "TOPRIGHT", 10, 0)
+		
+		_G.CharacterAttributesFrame:ClearAllPoints()
+		_G.CharacterAttributesFrame:SetPoint("CENTER", _G.CharacterFrame.backdrop, "CENTER", 0, -120)
+		
+		_G.CharacterNameFrame:ClearAllPoints()
+		_G.CharacterNameFrame:SetPoint("TOP", _G.CharacterFrame.backdrop, "TOP", 0, -10)
+		
+		_G.MagicResFrame1:ClearAllPoints()
+		_G.MagicResFrame1:SetPoint("TOPRIGHT", _G.CharacterHandsSlot, "TOPLEFT", -10, 0)
+	end
+end
+
+function KA:Initialize()
+	if not E.db.KlixUI.armory.enable or not E.private.skins.blizzard.character or IsAddOnLoaded('ElvUI_SLE') then return end
+
+	KA.db = E.db.KlixUI.armory
+
+	KUI:RegisterDB(self, "armory")
+    
+    local frame = T.CreateFrame("Frame", "KuiCharacterArmory", _G["PaperDollFrame"])
+    frame:SetFrameLevel(CharacterModelFrame:GetFrameLevel() - 1)
+    frame.BG = frame:CreateTexture(nil, "OVERLAY")
+    frame.BG:SetPoint("TOPLEFT", CharacterModelFrame, 0, 0)
+    frame.BG:SetPoint("BOTTOMRIGHT", CharacterModelFrame, 0, 0)
+	
+	KA:RegisterEvent("UPDATE_INVENTORY_DURABILITY", "UpdatePaperDoll", false)
+	KA:RegisterEvent("PLAYER_EQUIPMENT_CHANGED", "UpdatePaperDoll", false)
+	KA:RegisterEvent("COMBAT_RATING_UPDATE", "UpdatePaperDoll", false)
+	KA:RegisterEvent("PLAYER_ENTERING_WORLD", "InitialUpdatePaperDoll")
+	
+	self:ResizeCharacterFrame()
+	
+	if not E.db.general.itemLevel.displayCharacterInfo then
+		KA:UpdateIlvlFont()
+	end
+end
+
+KUI:RegisterModule(KA:GetName())
---
+++ ElvUI_KlixUI/modules/armory/options.lua
@@ -0,0 +1,330 @@
+local KUI, T, E, L, V, P, G = unpack(select(2, ...))
+local KA = KUI:GetModule("KuiArmory")
+
+local FontStyleList = {
+	["NONE"] = NONE,
+	["OUTLINE"] = 'OUTLINE',
+	["MONOCHROMEOUTLINE"] = 'MONOCROMEOUTLINE',
+	["THICKOUTLINE"] = 'THICKOUTLINE'
+}
+
+local function ArmoryTable()
+	E.Options.args.KlixUI.args.modules.args.armory = {
+		type = "group",
+		order = 4,
+		name = L["Armory"],
+		childGroups = 'tab',
+		get = function(info) return E.db.KlixUI.armory[ info[#info] ] end,
+		set = function(info, value) E.db.KlixUI.armory[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL"); end,
+		args = {
+			name = {
+				order = 1,
+				type = "header",
+				name = KUI:cOption(L["Armory"]),
+			},
+			info = {
+				order = 2,
+				type = "description",
+				name = L["ARMORY_DESC"],
+			},
+			enable = {
+				type = "toggle",
+				order = 3,
+				name = L["Enable"],
+				desc = L["Enable/Disable the |cfff960d9KlixUI|r Armory Mode."],
+				disabled = function() return T.IsAddOnLoaded("ElvUI_SLE") end,
+			},
+			gotoarmory = {
+				order = 4,
+				type = "execute",
+				name = L["ElvUI Armory"],
+				func = function() LibStub("AceConfigDialog-3.0-ElvUI"):SelectGroup("ElvUI", "general", "blizzUIImprovements") end,
+			},
+			space2 = {
+				order = 5,
+				type = "description",
+				name = "",
+			},
+			durability = {
+				order = 10,
+				type = "group",
+				name = L["Durability"],
+				disabled = function() return not E.db.KlixUI.armory.enable or T.IsAddOnLoaded("ElvUI_SLE") end,
+				get = function(info) return E.db.KlixUI.armory.durability[ info[#info] ] end,
+				set = function(info, value) E.db.KlixUI.armory.durability[ info[#info] ] = value; KA:UpdatePaperDoll() end,
+				args = {
+					enable = {
+						type = "toggle",
+						order = 1,
+						name = L["Enable"],
+						desc = L["Enable/Disable the display of durability information on the character window."],
+					},
+					onlydamaged = {
+						type = "toggle",
+						order = 2,
+						name = L["Damaged Only"],
+						desc = L["Only show durability information for items that are damaged."],
+						disabled = function() return not E.db.KlixUI.armory.durability.enable end,
+					},
+					space1 = {
+						order = 3,
+						type = "description",
+						name = "",
+					},
+					font = {
+						order = 4,
+						type = "select", dialogControl = "LSM30_Font",
+						name = L["Font"],
+						values = AceGUIWidgetLSMlists.font,
+						disabled = function() return not E.db.KlixUI.armory.durability.enable end,
+						set = function(info, value) E.db.KlixUI.armory.durability[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL") end, 
+					},
+					textSize = {
+						order = 5,
+						name = FONT_SIZE,
+						type = "range",
+						min = 6, max = 22, step = 1,
+						disabled = function() return not E.db.KlixUI.armory.durability.enable end,
+						set = function(info, value) E.db.KlixUI.armory.durability[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL") end, 
+					},
+					fontOutline = {
+						order = 6,
+						type = "select",
+						name = L["Font Outline"],
+						values = {
+							["NONE"] = NONE,
+							["OUTLINE"] = "OUTLINE",
+							["MONOCHROMEOUTLINE"] = "MONOCROMEOUTLINE",
+							["THICKOUTLINE"] = "THICKOUTLINE",
+						},
+						disabled = function() return not E.db.KlixUI.armory.durability.enable end,
+						set = function(info, value) E.db.KlixUI.armory.durability[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL") end, 
+					},
+				},
+			},
+			itemlevel = {
+				order = 11,
+				type = "group",
+				name = L["Itemlevel"],
+				--disabled = function() return not E.db.KlixUI.armory.enable or E.db.general.itemLevel.displayCharacterInfo or T.IsAddOnLoaded("ElvUI_SLE") end,
+				disabled = true,
+				get = function(info) return E.db.KlixUI.armory.ilvl[ info[#info] ] end,
+				set = function(info, value) E.db.KlixUI.armory.ilvl[ info[#info] ] = value; KA:UpdatePaperDoll() end,
+				args = {
+					enable = {
+						type = "toggle",
+						order = 1,
+						name = L["Enable"],
+						desc = L["Enable/Disable the display of item levels on the character window."],
+					},
+					space1 = {
+						order = 2,
+						type = "description",
+						name = "",
+					},
+					space2 = {
+						order = 3,
+						type = "description",
+						name = "",
+					},
+					font = {
+						order = 4,
+						type = "select", dialogControl = "LSM30_Font",
+						name = L["Font"],
+						values = AceGUIWidgetLSMlists.font,
+						disabled = function() return not E.db.KlixUI.armory.ilvl.enable end,
+						set = function(info, value) E.db.KlixUI.armory.ilvl[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL")  end, 
+					},
+					textSize = {
+						order = 5,
+						name = FONT_SIZE,
+						type = "range",
+						min = 6, max = 22, step = 1,
+						disabled = function() return not E.db.KlixUI.armory.ilvl.enable end,
+						set = function(info, value) E.db.KlixUI.armory.ilvl[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL")  end, 
+					},
+					fontOutline = {
+						order = 6,
+						type = "select",
+						name = L["Font Outline"],
+						values = {
+							["NONE"] = NONE,
+							["OUTLINE"] = "OUTLINE",
+							["MONOCHROMEOUTLINE"] = "MONOCROMEOUTLINE",
+							["THICKOUTLINE"] = "THICKOUTLINE",
+						},
+						disabled = function() return not E.db.KlixUI.armory.ilvl.enable end,
+						set = function(info, value) E.db.KlixUI.armory.ilvl[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL")  end, 
+					},
+					colorStyle = {
+						order = 7,
+						type = "select",
+						name = COLOR,
+						values = {
+							["RARITY"] = RARITY,
+							["LEVEL"] = L["Level"],
+							["CUSTOM"] = CUSTOM,
+						},
+						disabled = function() return not E.db.KlixUI.armory.ilvl.enable end,
+					},
+					color = {
+						order = 8,
+						type = "color",
+						name = COLOR_PICKER,
+						disabled = function() return E.db.KlixUI.armory.ilvl.colorStyle == "RARITY" or E.db.KlixUI.armory.ilvl.colorStyle == "LEVEL" or not E.db.KlixUI.armory.ilvl.enable end,
+						get = function(info)
+							local t = E.db.KlixUI.armory.ilvl[ info[#info] ]
+							local d = P.KlixUI.armory.ilvl[info[#info]]
+							return t.r, t.g, t.b, t.a, d.r, d.g, d.b
+						end,
+						set = function(info, r, g, b)
+							E.db.KlixUI.armory.ilvl[ info[#info] ] = {}
+							local t = E.db.KlixUI.armory.ilvl[ info[#info] ]
+							t.r, t.g, t.b, t.a = r, g, b, a
+							KA:UpdatePaperDoll()
+						end,
+					},
+				},
+			},
+			--[[indicators = {
+				order = 14,
+				type = "group",
+				name = L["Indicators"],
+				disabled = function() return not E.db.KlixUI.armory.enable or T.IsAddOnLoaded("ElvUI_SLE") end,
+				args = {
+					enchant = {
+						order = 1,
+						type = "group",
+						name = L["Enchant"],
+						disabled = function() return E.db.general.itemLevel.displayCharacterInfo end,
+						get = function(info) return E.db.KlixUI.armory.indicators.enchant[ info[#info] ] end,
+						set = function(info, value) E.db.KlixUI.armory.indicators.enchant[ info[#info] ] = value; KA:UpdatePaperDoll() end,
+						args = {
+							enable = {
+								type = "toggle",
+								order = 1,
+								name = L["Enable"],
+								desc = L["Shows an indictor for enchanted/not enchanted items."],
+							},
+							glow = {
+								order = 2,
+								type = "group",
+								name = L["Glow Indicator"],
+								guiInline = true,
+								disabled = function() return not E.db.KlixUI.armory.indicators.enchant.enable end,
+								get = function(info) return E.db.KlixUI.armory.indicators.enchant.glow[ info[#info] ] end,
+								set = function(info, value) E.db.KlixUI.armory.indicators.enchant.glow[ info[#info] ] = value; E:StaticPopup_Show("PRIVATE_RL") end,
+								args = {
+									enable = {
+										order = 1,
+										type = "toggle",
+										name = L["Enable"],
+										desc = L["Shows a glow indicator of not enchanted items only."],
+									},
+									style = {
+										order = 2,
+										type = "select",
+										name = L["Style"],
+										values = {
+											["Button"] = L["Button Glow"],
+											["Pixel"] = L["Pixel Glow"],
+											["AutoCast"] = L["AutoCast Glow"],
+										},
+										disabled = function() return not E.db.KlixUI.armory.indicators.enchant.glow.enable end,
+										set = function(info, value) E.db.KlixUI.armory.indicators.enchant.glow.style = value; KA:UpdatePaperDoll() end,
+									},
+									color = {
+										order = 3,
+										type = "color",
+										name = L["Color"],
+										hasAlpha = true,
+										disabled = function() return not E.db.KlixUI.armory.indicators.enchant.glow.enable end,
+										get = function(info)
+											local t = E.db.KlixUI.armory.indicators.enchant.glow.color
+											return t.r, t.g, t.b, t.a
+										end,
+										set = function(info, r, g, b, a)
+											local t = E.db.KlixUI.armory.indicators.enchant.glow.color
+											t.r, t.g, t.b, t.a = r, g, b, a
+											KA:UpdatePaperDoll()
+										end,
+									},
+								},
+							},
+						},
+					},
+				},
+			},]]
+			gradient = {
+				order = 15,
+				type = 'group',
+				name = L["Gradient"],
+				disabled = function() return not E.db.KlixUI.armory.enable or T.IsAddOnLoaded("ElvUI_SLE") end,
+				get = function(info) return E.db.KlixUI.armory.gradient[ info[#info] ] end,
+				set = function(info, value) E.db.KlixUI.armory.gradient[ info[#info] ] = value; KA:UpdatePaperDoll() end,
+				args = {
+					enable = {
+						type = 'toggle',
+						name = L["Enable"],
+						order = 1,
+					},
+					colorStyle = {
+						order = 2,
+						type = "select",
+						name = COLOR,
+						values = {
+							["RARITY"] = RARITY,
+							["VALUE"] = L["Value"],
+							["CUSTOM"] = CUSTOM,
+						},
+					},
+					color = {
+						order = 3,
+						type = "color",
+						name = COLOR_PICKER,
+						disabled = function() return E.db.KlixUI.armory.gradient.colorStyle == "RARITY" or E.db.KlixUI.armory.gradient.colorStyle == "VALUE" or not E.db.KlixUI.armory.gradient.enable end,
+						get = function(info)
+							local t = E.db.KlixUI.armory.gradient[ info[#info] ]
+							local d = P.KlixUI.armory.gradient[info[#info]]
+							return t.r, t.g, t.b, d.r, d.g, d.b
+						end,
+						set = function(info, r, g, b)
+							E.db.KlixUI.armory.gradient[ info[#info] ] = {}
+							local t = E.db.KlixUI.armory.gradient[ info[#info] ]
+							t.r, t.g, t.b = r, g, b
+							KA:UpdatePaperDoll()
+						end,
+					},
+					alpha = {
+						order = 4,
+						type = "range",
+						name = L["alpha"],
+						min = 0, max = 1, step = .1,
+					},
+				},
+			},
+		},
+	}
+end
+T.table_insert(KUI.Config, ArmoryTable)
+
+local function injectElvUIArmoryOptions()
+	E.Options.args.general.args.blizzUIImprovements.args.itemLevelInfo.args.displayCharacterInfo = {
+		order = 1,
+		type = "toggle",
+		name = L["Display Character Info"],
+		desc = L["Shows item level of each item, enchants, and gems on the character page."],
+		set = function(info, value)
+			E.db.general.itemLevel.displayCharacterInfo = value;
+			E:GetModule('Misc'):ToggleItemLevelInfo(); E:StaticPopup_Show("PRIVATE_RL")
+		end
+	}
+	
+	E.Options.args.general.args.blizzUIImprovements.args.itemLevelInfo.args.gotoklixui = {
+		order = 2,
+		type = "execute",
+		name = KUI:cOption(L["KlixUI Armory"]),
+		func = function() LibStub("AceConfigDialog-3.0-ElvUI"):SelectGroup("ElvUI", "KlixUI", "modules", "armory") end,
+	}
+end
+T.table_insert(KUI.Config, injectElvUIArmoryOptions)
