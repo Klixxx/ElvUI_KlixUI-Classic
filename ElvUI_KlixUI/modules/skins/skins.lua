d881e11e74531776fb37f9046ea1e7ad3b8d778e
Klix <33373965+Klixxx@users.noreply.github.com>
2019-09-09 19:36:03 +0200

    optimize the XTolevel skin

--- ElvUI_KlixUI/modules/skins/skins.lua
+++ ElvUI_KlixUI/modules/skins/skins.lua
@@ -1064,46 +1064,33 @@ local function styleAddons()
 	
 	-- XToLevel
 	if T.IsAddOnLoaded("XToLevel") then
-		if XToLevel_AverageFrame_Blocky_PlayerFrameCounterKills then
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterKillsBackground:StripTextures()
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterKillsBackground:CreateBackdrop("Transparent")
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterKillsBackground.backdrop:Styling()
-		end
-		if XToLevel_AverageFrame_Blocky_PlayerFrameCounterQuests then
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterQuestsBackground:StripTextures()
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterQuestsBackground:CreateBackdrop("Transparent")
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterQuestsBackground.backdrop:Styling()
-		end
-		if XToLevel_AverageFrame_Blocky_PlayerFrameCounterDungeons then
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterDungeonsBackground:StripTextures()
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterDungeonsBackground:CreateBackdrop("Transparent")
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterDungeonsBackground.backdrop:Styling()
-		end
-		if XToLevel_AverageFrame_Blocky_PlayerFrameCounterBattles then
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterBattlesBackground:StripTextures()
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterBattlesBackground:CreateBackdrop("Transparent")
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterBattlesBackground.backdrop:Styling()
-		end
-		if XToLevel_AverageFrame_Blocky_PlayerFrameCounterObjectives then
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterObjectivesBackground:StripTextures()
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterObjectivesBackground:CreateBackdrop("Transparent")
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterObjectivesBackground.backdrop:Styling()
-		end
-		if XToLevel_AverageFrame_Blocky_PlayerFrameCounterProgress then
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterProgressBackground:StripTextures()
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterProgressBackground:CreateBackdrop("Transparent")
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterProgressBackground.backdrop:Styling()
-		end
-		if XToLevel_AverageFrame_Blocky_PlayerFrameCounterTimer then
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterTimerBackground:StripTextures()
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterTimerBackground:CreateBackdrop("Transparent")
-			XToLevel_AverageFrame_Blocky_PlayerFrameCounterTimerBackground.backdrop:Styling()
+		local frame = _G.XToLevel_AverageFrame_Classic
+		if frame then
+			frame:StripTextures()
+			frame:CreateBackdrop("Transparent")
+			frame.backdrop:Styling()
 		end
 		
-		if XToLevel_AverageFrame_Classic then
-			XToLevel_AverageFrame_Classic:StripTextures()
-			XToLevel_AverageFrame_Classic:CreateBackdrop("Transparent")
-			XToLevel_AverageFrame_Classic.backdrop:Styling()
+		local XToLevelFrames = {
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterKillsBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterQuestsBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterDungeonsBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterBattlesBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterObjectivesBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterPetBattlesBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterGatheringBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterDigsBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterProgressBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterTimerBackground,
+			_G.XToLevel_AverageFrame_Blocky_PlayerFrameCounterGuildProgressBackground,
+		}
+
+		for _, blockFrame in T.pairs(XToLevelFrames) do
+			if blockFrame then
+				blockFrame:StripTextures()
+				blockFrame:CreateBackdrop("Transparent")
+				blockFrame.backdrop:Styling()
+			end
 		end
 	end
 	
